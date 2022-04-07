29ad73e39313a2d874acb64cdefce09bc5319538
JeongHeon Woo <contact@wookingwoo.com>
2022-04-08 05:23:19 +0900

    Change string to website (back-end data)

--- src/pages/oj/components/NavBar.vue
+++ src/pages/oj/components/NavBar.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="header">
     <Menu theme="light" mode="horizontal" @on-select="handleRoute" :active-name="activeMenu" class="oj-menu">
-      <!-- <div class="logo"><span>{{website.website_name}}</span></div> -->
-      <div class="logo"><span>Wookingwoo Online Judge</span></div>
+      <div class="logo"><span>{{website.website_name}}</span></div>
       <Menu-item name="/">
         <Icon type="home"></Icon>
         {{$t('m.Home')}}
